b42c291f0a775e5481f5d69bd7fe5ae2df0a2f20
saurabhtiwariGLA <92859951+saurabhtiwariGLA@users.noreply.github.com>
2022-05-31 09:53:02 +0530

    Create q10.py

---
+++ numpy/q10.py
@@ -0,0 +1,17 @@
+import numpy as np
+a = np.array([1,2,5])
+b = np.array([2,1,0])
+print("Original 1-d arrays:")
+print(a)
+print(b)
+print 
+result = np.inner(a, b)
+print("Inner product of the said vectors:")
+x = np.arange(9).reshape(3, 3)
+y = np.arange(3, 12).reshape(3, 3)
+print("Higher dimension arrays:")
+print(x)
+print(y)
+result = np.inner(x, y)
+print("Inner product of the said vectors:")
+print(result)
